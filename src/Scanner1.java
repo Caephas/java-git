3a2b9ff329daf8e74a9d23d41d46a313f71991a0
Caephas <don.b.peterpaul@gmail.com>
2019-12-02 11:47:40 +0100

    First Commit

---
+++ src/Scanner1.java
@@ -0,0 +1,15 @@
+// Prompt the user to enter an integer
+import java.util.Scanner;
+public class Scanner1 {
+
+	public static void main(String[] args) {
+		Scanner bobby = new Scanner (System.in);
+		System.out.print("Enter first number: " );
+		int num1 = bobby.nextInt();
+		System.out.print("Enter second number: ");
+		int num2 = bobby.nextInt();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Scanner2.java
@@ -0,0 +1,17 @@
+//Input an integer and assign the result to int variable value.
+//Assume Scanner variable input can be used to read a value
+//from the keyboard.
+import java.util.Scanner;
+public class Scanner2 {
+	public static void main(String []args) {
+		Scanner zaga = new Scanner (System.in);
+		System.out.print("Enter first number: ");
+		int num1 = zaga.nextInt();
+		System.out.print("Enter Second number: ");
+		int num2 = zaga.nextInt();
+		int sum = num1 + num2;
+		System.out.println("The sum is " + sum);
+		
+	}
+
+}
