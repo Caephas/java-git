ffb0c378b91f2e99281f1a86fd325821f3384f06
Caephas <don.b.peterpaul@gmail.com>
2019-12-02 11:25:36 +0100

    First Commit

---
+++ src/Conditionals.java
@@ -0,0 +1,13 @@
+//If the variable number is not equal to 7, display "The variable number is not equal to 7".
+public class Conditionals {
+
+	public static void main(String[] args) {
+		int number = 6;
+		if (number != 7) {
+			System.out.println("The variable number is not equal to 7");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
