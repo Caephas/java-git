9431e12d8dff7a040570bf5f1df12f93d1d4b68b
Caephas <42560887+Caephas@users.noreply.github.com>
2019-12-02 10:03:53 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Welcome to java programming");
+		System.out.println("Github isn't bad ");
 		// TODO Auto-generated method stub
 
 	}
