b349cb65e08037d45b64c4022fdc8cd07b752cac
Caephas <don.b.peterpaul@gmail.com>
2019-12-02 10:47:27 +0100

    First Commit

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+// Declare variables c,thisIsVariable,q76354 and number to be of type int and initialize each to 0.
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int c,thisIsVariable,q76354,number;
+		c = 0;
+		thisIsVariable = 0;
+		q76354 = 0;
+		number = 0;
+// or
+		int c1 = 0;
+		int thisIsVariable1 = 0;
+		int q763544 = 0;
+		int number1 = 0;
+		// TODO Auto-generated method stub
+
+	}
+
+}
